5f2db05323f539a50805e8a1d788f24bf3bdcef8
Ferdinand Lemaire <ferdinand.lemaire@epita.fr>
2018-05-06 17:09:35 +0200

    Add - Argument checking

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,29 @@
 #include "main.hh"
+#include <fstream>
 
-int main()
+int main(int argv, char* argc[])
 {
+  if (argv != 3)
+  {
+    std::cerr << "Wrong number of arguments." << std::endl;
+    return 1;
+  }
+  std::ifstream inp_dic (argc[1]);
+  if (!inp_dic.is_open())
+  {
+    std::cerr << argc[1] << " Is not a valid file" << std::endl;
+    return 1;
+  }
+
+  std::ifstream inp_log (argc[2]);
+  if (!inp_log.is_open())
+  {
+    std::cerr << argc[2] << " Is not a valid file" << std::endl;
+    return 1;
+  }
+
+
+  inp_dic.close();
+  inp_log.close();
   return 0;
 }
--- src/main.hh
+++ src/main.hh
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <fstream>
 
 std::unordered_map<std::string,int> fill_words();
