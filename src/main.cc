25e258f035cb802dbfa8a88217aa25f6d0685700
Ferdinand Lemaire <ferdinand.lemaire@epita.fr>
2018-05-06 17:38:14 +0200

    Add - Function to get dict of word from file

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,29 @@
 #include "main.hh"
-#include <fstream>
+#include <err.h>
+
+std::unordered_map<std::string,int> fill_words(std::ifstream input_file)
+{
+  std::string current;
+  std::unordered_map<std::string,int> res;
+  while(getline(input_file,current))
+  {
+    unsigned int i = 0;
+    std::string behaviour;
+    std::string word;
+    for (; current.at(i) != ' '; ++i)
+      behaviour += current.at(i);
+    ++i;
+    for (; i < current.length(); ++i)
+      word += current.at(i);
+
+    int karma = std::stoi(behaviour);
+    if (karma < 5 || karma > 5)
+      err(1,"Input dictionnary file not well formated");
+    else
+      res.insert({word, karma});
+  }
+  return res;
+}
 
 int main(int argv, char* argc[])
 {
--- src/main.hh
+++ src/main.hh
@@ -3,4 +3,4 @@
 #include <unordered_map>
 #include <fstream>
 
-std::unordered_map<std::string,int> fill_words();
+std::unordered_map<std::string,int> fill_words(std::ifstream input_file);
